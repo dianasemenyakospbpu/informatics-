f41d201e49eb2a11eb6dc4dad1f80895c9a2a828
Семеняко Диана Дмитриевна <diana.semenyako@gmail.com>
2026-02-22 18:29:28 +0300

    Update task_2-0_6.py

--- projects_2/task_2_0/task_2-0_6.py
+++ projects_2/task_2_0/task_2-0_6.py
@@ -1,3 +1,4 @@
-f = open ('C:\\Users\\User\\Desktop\\инфа.\\output.txt', 'w', encoding="utf-8")
+f = open ('C:\\Users\\User\\Desktop\\222\\output.txt', 'w', encoding="utf-8")
 print ('Имя: Диана Семеняко\nКурс: 1\nГруппа: 4731204/50003', file=f)
 f.close()
+
