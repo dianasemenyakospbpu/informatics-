c36e9276f9a4e962996172e38368bd57eb181728
Семеняко Диана Дмитриевна <diana.semenyako@gmail.com>
2026-05-13 00:41:43 +0300

    Add files via upload

---
+++ projects_6/task_6_0/task_6_0-1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\User\Documents\semenyako_dd\projects_6\task_6_0\wild_boars.csv')
+
+print("Длины клыков:")
+print(df['tusk_length_cm'])
+
+min_tusk = df['tusk_length_cm'].min()
+max_tusk = df['tusk_length_cm'].max()
+
+print(f"\nСамая короткая длина клыков: {min_tusk} см")
+print(f"Самая длинная длина клыков: {max_tusk} см")
